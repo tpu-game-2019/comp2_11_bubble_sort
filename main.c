e0586a8c281996753277bcb1d704f03650ac6158
Kuryu86 <Kuryu86+github@gmail.com>
2020-01-05 22:09:20 +0900

    バブルソートの実装と表示

--- main.c
+++ main.c
@@ -1,12 +1,57 @@
 #include <stdio.h>
-
-int main(int argc, char *argv[]) 
+#include <stdlib.h>
+#define ArrayMax 100
+int main(int argc, char *argv[])
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
+	char str[7];
+	int Array[ArrayMax] = {};
+	int count = 0;
+	while (fgets(str, sizeof(str), stdin))
+	{
+		Array[count] = atoi(str);
+		count++;
+	}
+	
+#pragma region
+	int IndexMax = count;
+	count = 0;
+	int MinIndex = 0;
+	for (size_t i = 0; i < IndexMax; i++)
+	{
+		printf("%d", Array[i]);
+		if (i+1 < IndexMax)
+			printf(" ");
+		else
+			printf("\n");
+	}
+	for (int j = 0; j < IndexMax; j++)
+	{
+		int Min = Array[j];
+		for (size_t i = j; i < IndexMax; i++)
+		{
+			if (Min > Array[i])
+			{
+				Min = Array[i];
+				MinIndex = i;
+			}
+		}
+		while (MinIndex > 0 && Array[MinIndex] < Array[MinIndex - 1])
+		{
+			int tmp = Array[MinIndex];
+			Array[MinIndex] = Array[MinIndex - 1];
+			Array[MinIndex - 1] = tmp;
+			MinIndex--;
+			for (size_t i = 0; i < IndexMax; i++)
+			{
+				printf("%d", Array[i]);
+				if (i+1 < IndexMax)
+					printf(" ");
+				else
+					printf("\n");
+			}
+		}
+	}
+#pragma endregion
 
-    return 0;
+	return 0;
 }
