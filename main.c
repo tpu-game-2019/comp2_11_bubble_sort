2b5c955decbc54d361518e8d408c22c33f19bbbe
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2020-01-26 19:31:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
-
-	int i, j, tmp;
-
 	char str[7];
 	int number[100];
-	int count=0;
-	
+	int i, j, count = 0;
+
 	while (fgets(str, sizeof(str), stdin)) {
 		number[count] = atoi(str);
 		count++;
 	}
 
+	//表示
 	for (i = 0; i < count; i++) {
 		printf("%d", number[i]);
 		if (i == count - 1)
@@ -22,12 +20,16 @@ int main(void)
 			putchar(' ');
 	}
 
-	for (i = 0; i < count; ++i) {
-		for (j = i + 1; j < count; ++j) {
-			if (number[i] > number[j]) {
-				tmp = number[i];
-				number[i] = number[j];
-				number[j] = tmp;
+	int swap;
+
+	for (i = 0; i < count; i++) {
+		for (j = count - 1; j > 0; j--) {
+			if (number[j] < number[j - 1]) {
+				//交換
+				swap = number[j];
+				number[j] = number[j - 1];
+				number[j - 1] = swap;
+				//表示
 				for (i = 0; i < count; i++) {
 					printf("%d", number[i]);
 					if (i == count - 1)
