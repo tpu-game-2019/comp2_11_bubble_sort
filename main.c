37860d10b9dc2f42fe34376e3deb7d81ae6adb11
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2020-01-26 19:17:39 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
+int main(void)
 {
-    char str[7];
-    
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
-    }
 
-    return 0;
+	int i, j, tmp;
+
+	char str[7];
+	int number[100];
+	int count=0;
+	
+	while (fgets(str, sizeof(str), stdin)) {
+		number[count] = atoi(str);
+		count++;
+	}
+
+	for (i = 0; i < count; ++i) {
+		for (j = i + 1; j < count; ++j) {
+			if (number[i] > number[j]) {
+				tmp = number[i];
+				number[i] = number[j];
+				number[j] = tmp;
+			}
+		}
+		for(i = 0; i < count; ++i)
+			printf("%d\n", number[i]);
+	}
+
+	for (i = 0; i < count; ++i)
+		printf("%d\n", number[i]);
+
+	printf("\n");
+
+	return 0;
 }
