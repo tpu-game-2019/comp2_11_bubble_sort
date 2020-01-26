28ef8df4cf7b2c4c32ad4b03772ec2c8c067f9bb
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2020-01-26 19:43:02 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,43 +1,42 @@
 #include <stdio.h>
 
-void Swap(int*a,int*b)
+int main(int argc, char* argv[])
 {
-    int temp;
-    temp=*a;*a=*b;*b=temp;
+	char str[7];
+	int data[100];
+	int i, j, count = 0;
+	int temp;
+
+	while (fgets(str, sizeof(str), stdin)) {
+		data[count] = atoi(str);
+		count++;
+	}
+
+	for (i = 0; i < count; i++) {
+		printf("%d", data[i]);
+		if (i == count - 1)
+			putchar('\n');
+		else
+			putchar(' ');
+	}
+		for (i = 0; i < count; i++) {
+			for (j = count - 1; j > 0; j--) {
+				if (data[j] < data[j - 1]) {
+					temp = data[j];
+					data[j] = data[j - 1];
+					data[j - 1] = temp;
+
+					for (i = 0; i < count; i++) {
+						printf("%d", data[i]);
+						if (i == count - 1)
+							putchar('\n');
+						else
+							putchar(' ');
+					}
+				}
+			}
+		}
+	
+	return 0;
 }
-
-void ShowArray(int*data,int n)
-{
-    int i;
-    for(i=0;i<n;i++){
-        printf("%d",data[i]);
-        if(i==n-1)
-            putchar('\n');
-        else
-            putchar(' ');
-    }
-}
-int main(int argc, char *argv[]) 
-{
-    char str[7];
-    int data[100];
-    int i,j,count=0;
-
-    while(fgets(str, sizeof(str), stdin)){
-       data[count]=atoi(str);
-        count++;
-    }
-
-    ShowArray(data,count);
-
-    for(i=0;i<count;i++){
-        for(j=count-1;j>0;j--){
-            if(data[j]<data[j-1]){
-                Swap(&data[j],&data[j-1]);
-                ShowArray(data,count);
-            }
-        }
-    }
-
-    return 0;
-}
+		
