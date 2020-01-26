766256af3a6b31937d6b9583482cc564f2c6ec4e
simizu0618 <55948164+simizu0618@users.noreply.github.com>
2020-01-26 20:25:00 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,17 @@
 int main(int argc, char* argv[])
 {
 	char str[7];
-	int data[100];
+	int number[100];
 	int i, j,n, count = 0;
 	int temp;
 
 	while (fgets(str, sizeof(str), stdin)) {
-		data[count] = atoi(str);
+		number[count] = atoi(str);
 		count++;
 	}
 
 	for (n = 0; n < count; n++) {
-		printf("%d", data[n]);
+		printf("%d", number[n]);
 		if (n == count - 1)
 			putchar('\n');
 		else
@@ -22,10 +22,10 @@ int main(int argc, char* argv[])
 
 	for (i = 0; i < count; i++) {
 		for (j = count - 1; j > 0; j--) {
-			if (data[j] < data[j - 1]) {
-				temp = data[j]; data[j] = data[j - 1]; data[j - 1] = temp;
+			if (number[j] < number[j - 1]) {
+				temp = number[j]; number[j] = number[j - 1]; number[j - 1] = temp;
 				for (n = 0; n < count; n++) {
-					printf("%d", data[n]);
+					printf("%d", number[n]);
 					if (n == count - 1)
 						putchar('\n');
 					else
